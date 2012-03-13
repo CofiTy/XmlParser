54878be05aa33781a1c61eacc3eef27efbe18bfd
mat <sabourin.mathieu@free.fr>
2012-03-13 11:39:11 +0100

    Added data structures for XML analyser.

---
+++ AnalyseurXML/NodeList.h
@@ -0,0 +1,25 @@
+#ifndef H_NODE_L
+#define H_NODE_L
+
+#include <map>
+#include <list>
+#include <string>
+
+#include "Node.h"
+
+class NodeList : public Node
+{
+public:
+  //NodeList(std::string tagName);
+  std::string tagName;
+  std::string nameSpace;
+  std::map<std::string, std::string> attributes;
+  std::list<Node> childNodeList;
+  
+  std::string getChildNodesString();
+  std::list<std::string> getAttributesList();
+  void addAttribute(std::string key, std::string value);
+  void appendChild(Node & node);
+};
+
+#endif
